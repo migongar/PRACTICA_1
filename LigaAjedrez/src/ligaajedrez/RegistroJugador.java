c7cd537850ac4b33cbc24eaced17e5eab6f17619
angel68764 <72401575+angel68764@users.noreply.github.com>
2020-10-27 12:52:05 +0100

    clases registro y inscribir despues modelo Edward

--- LigaAjedrez/src/ligaajedrez/RegistroJugador.java
+++ LigaAjedrez/src/ligaajedrez/RegistroJugador.java
@@ -221,7 +221,7 @@ public class RegistroJugador extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        String dni, nom, ape, club;
+        String dni, nom, ape;
         int edad;
         Calendar hoy = Calendar.getInstance();
 
